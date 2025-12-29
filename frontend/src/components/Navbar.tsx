3f2ab2bf2a39ca8c4147dfe39295d5404a0e2ab3
samadou <mossabm036@gmail.com>
2025-12-29 19:41:16 +0100

    style: enhancing the hero section

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 z-50 w-full transition-all duration-300 ${scrolled ? "bg-[#0A1633] shadow-lg" : "md:bg-opacity-90 bg-transparent"}`}
+      className={`fixed top-0 z-50 w-full bg-[#0A1633] shadow-lg transition-all duration-300`}
     >
       <div
         className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ${!scrolled && "bg-transparent"}`}
